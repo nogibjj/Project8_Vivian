9dc97edac83a9e11104b4e47bedeb95af897e8a0
vivianzzzzz <143654445+vivianzzzzz@users.noreply.github.com>
2023-10-20 14:47:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,19 @@ from lib.CRUD import (
     create_house,
     update_house,
     delete_house,
-    read_house_by_id
+    read_house_by_id,
+    read_all_houses
 )
 from lib.load_data import load
 
+def count_houses():
+    houses = read_all_houses()
+    return len(houses)
 
 def main():
     load("california_housing_train.csv")  # Import data from CSV
-
-    #create_house
-    create_house(-122.5, 37.7, 40, 2000, 350, 1500, 350, 8.5, 500000)  # Create
-
-    # update house with id=1
-    update_house(1, -122.5, 37.7, 45, 2500, 400, 1600, 400, 9.0, 550000)  # Update
-
-    # print house with id=10
-    read_house_by_id(10)  # Read after update
-
-    # delete house with id=5
-    delete_house(1)  # Delete
+    number_of_houses = count_houses()
+    print(f"There are {number_of_houses} houses in the database.")
 
     # Close the connection
     close_connection()
