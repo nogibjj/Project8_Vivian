905dca34a78dee20d0ef3ba9635ab4b9d0c0555c
vivianzzzzz <143654445+vivianzzzzz@users.noreply.github.com>
2023-10-20 15:14:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-from lib.CRUD import (
-    close_connection,
-    create_house,
-    update_house,
-    delete_house,
-    read_house_by_id,
-    read_all_houses
-)
-from lib.load_data import load
+import pandas as pd
+
+def read_all_houses():
+    return pd.read_csv("california_housing_train.csv")
 
 def count_houses():
     houses = read_all_houses()
     return len(houses)
 
 def main():
-    load("california_housing_train.csv")  # Import data from CSV
     number_of_houses = count_houses()
-    print(f"There are {number_of_houses} houses in the database.")
-
-    # Close the connection
-    close_connection()
+    print(f"There are {number_of_houses} houses in the dataset.")
     return 1
-
 
 if __name__ == "__main__":
     main()
+
