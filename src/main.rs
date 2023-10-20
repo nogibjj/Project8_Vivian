b0dfb7141f7ded79c06b97687a13f90869045dbc
vivianzzzzz <143654445+vivianzzzzz@users.noreply.github.com>
2023-10-20 15:48:42 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
-use std::error::Error;
 use std::fs::File;
-
 use csv::Reader;
+use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let number_of_houses = count_houses()?;
     println!("There are {} houses in the dataset.", number_of_houses);
+
+    let total = sum_median_house_values()?;
+    println!("Total of all median house values: {}", total);
+
     Ok(())
 }
 
 fn count_houses() -> Result<usize, Box<dyn Error>> {
     let file = File::open("california_housing_train.csv")?;
+    let rdr = Reader::from_reader(file);
+    Ok(rdr.records().count())
+}
+
+fn sum_median_house_values() -> Result<f64, Box<dyn Error>> {
+    let file = File::open("california_housing_train.csv")?;
     let mut rdr = Reader::from_reader(file);
-    let count = rdr.records().count();
-    Ok(count)
+    let mut total = 0.0;
+
+    for result in rdr.records() {
+        let record = result?;
+        let value: f64 = record[8].parse()?;
+        total += value;
+    }
+
+    Ok(total)
 }
