fcd4b8c4d100d92a2d5c59451652da70bef27f13
vivianzzzzz <143654445+vivianzzzzz@users.noreply.github.com>
2023-10-20 15:51:13 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 use std::fs::File;
 use csv::Reader;
 use std::error::Error;
+use std::time::Instant;
+use sys_info;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let start = Instant::now();
+    let mem_info_before = sys_info::mem_info().unwrap();
+
     let number_of_houses = count_houses()?;
     println!("There are {} houses in the dataset.", number_of_houses);
 
     let total = sum_median_house_values()?;
     println!("Total of all median house values: {}", total);
+    
+    let elapsed = start.elapsed();
+    let mem_info_after = sys_info::mem_info().unwrap();
+    let mem_used = mem_info_after.total - mem_info_before.total;
+
+    std::fs::write(
+        "rust_performance.md",
+        format!(
+            "## Rust Performance Report\n- Time taken: {:.2?} seconds\n- Memory used: {} KB\n### Operations Performed\n1. Read CSV file\n2. Count number of houses\n3. Sum median house values\n", 
+            elapsed, mem_used
+        )
+    )?;
 
     Ok(())
 }
+
 
 fn count_houses() -> Result<usize, Box<dyn Error>> {
     let file = File::open("california_housing_train.csv")?;
