b0c9014b774ff023c1dab9d9cb38e0d98b8b756e
vivianzzzzz <143654445+vivianzzzzz@users.noreply.github.com>
2023-10-20 22:02:33 -0400

    Update test_main.rs

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -1,11 +1,8 @@
-#[cfg(test)]
-mod tests {
-    use super::super::*;
+use count_and_sum_price;
 
-    #[test]
-    fn test_program_success() {
-        let result = run_program().unwrap();
-        assert_eq!(result, 1);
-    }
+#[test]
+fn test_program_success() {
+    let result = your_project_name::run_program().unwrap();
+    assert_eq!(result, 1);
 }
 
