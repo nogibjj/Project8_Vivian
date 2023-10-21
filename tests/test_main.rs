19aa9bf93bff0878dde5b44e0373c359aa04eb2d
vivianzzzzz <143654445+vivianzzzzz@users.noreply.github.com>
2023-10-20 21:44:30 -0400

    Update test_main.rs

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -1,30 +1,11 @@
-// test_main.rs
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_count_houses() {
-        let data = "
-            longitude,latitude,housing_median_age,total_rooms,total_bedrooms,population,households,median_income,median_house_value
-            -122.23,37.88,41.0,880.0,129.0,322.0,126.0,8.3252,452600.0
-            -122.22,37.86,21.0,7099.0,1106.0,2401.0,1138.0,8.3014,358500.0
-        ";
-        let file = File::from(data);
-        let count = count_houses(file).unwrap();
-        assert_eq!(count, 2);
-    }
-
-    #[test]
-    fn test_sum_median_house_values() {
-        let data = "
-            longitude,latitude,housing_median_age,total_rooms,total_bedrooms,population,households,median_income,median_house_value
-            -122.23,37.88,41.0,880.0,129.0,322.0,126.0,8.3252,452600.0
-            -122.22,37.86,21.0,7099.0,1106.0,2401.0,1138.0,8.3014,358500.0
-        ";
-        let file = File::from(data);
-        let sum = sum_median_house_values(file).unwrap();
-        assert_eq!(sum, 452600.0 + 358500.0);
+    fn test_program_success() {
+        let result = run_program().unwrap();
+        assert_eq!(result, 1);
     }
 }
+
